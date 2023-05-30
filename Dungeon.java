a78904583d1f77002b8c96e0666096e66e3efe78
NISHADYHHS <62825462+NISHADYHHS@users.noreply.github.com>
2023-05-29 21:57:25 -0700

    Update Dungeon.java

--- Dungeon.java
+++ Dungeon.java
@@ -1,4 +1,41 @@
+import java.util.ArrayList; // import the ArrayList class
 
 public class Dungeon {
+	
+	String name;// will be something like "LEVEL 1...2...3...etc"
+	int difficulty;// the difficulty of the dungeon
+	ArrayList<Character> monsterList;// the list of monsters in this dungeon
+	
+	void spawnList(){// sets the monsterlist that the dungeon has based on the difficulty of the dungeon
+		
+		if(difficulty == 1)
+			//
+		else if(difficulty == 2)
+			//	
+		else if(difficulty == 3)
+			//etc...
+			
 
+		
+	}
+	
+	void monsterDefeated() {// when the main character beats a monster, it is removed from the arraylist
+		
+		monsterList.remove(0);
+	}
+	
+	void setDungeon(ArrayList<Character> monsters) {// set method
+		
+		monsterList = monsters;
+	}
+	
+	ArrayList<Character> getDungeon(){// get method
+		
+		return monsterList;
+	}
+	
+	
+	
+	
+	
 }
