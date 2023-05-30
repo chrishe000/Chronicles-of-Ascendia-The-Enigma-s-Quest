8d447ac1d40216a7fce4870f15e778cb102e2298
chrishe000 <134239835+chrishe000@users.noreply.github.com>
2023-05-29 18:03:28 -0700

    Add files via upload

--- Item.java
+++ Item.java
@@ -1,4 +1,48 @@
+// Author: Chris He
+// Date: 5/29/2023
+/*
+AdventureGameItem sword = new AdventureGameItem("Sword", "A sharp weapon for battles");
+System.out.println(sword.getName()); // Output: Sword
+System.out.println(sword.getDescription()); // Output: A sharp weapon for battles
+
+sword.use();    // Output: Using Sword...
+*/
 
 public class Item {
+    private String name;
+    private String description;
+    private int cost;
 
+    public Item(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    
+    public int getCost() {
+        return cost;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public void use() {
+        System.out.println("Using " + name + "...");
+    }
 }
