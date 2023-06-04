18d8f677620349ffdb931bee6234825536e282bb
NISHADYHHS <62825462+NISHADYHHS@users.noreply.github.com>
2023-06-04 11:59:43 -0700

    Update Enemy.java

--- Enemy.java
+++ Enemy.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class Enemy {
 
@@ -6,24 +7,50 @@ public class Enemy {
 	int defense;
 	String name;
 	int hp;
+	ArrayList<Item> drops;
+	boolean isAlive;
 	
-	public Enemy(int attack, int defense, String name, int hp) {
+	public Enemy(int attack, int defense, String name, int hp, ArrayList<Item> drops) {
 		
 		this.attack = attack;
 		this.defense = defense;
 		this.name = name;
 		this.hp = hp;
+		this.drops = drops;
 		
 	}
 	
-	int attackPlayer(Character player, int attack) {
+	int attackPlayer(MainCharacter player, int attack) {
 		
 		int damage = player.getDefense() - attack;
 		return damage;
 	}
 	
+	void die() {
+		if(hp == 0)
+			isAlive = false;
+	}
+	
+	void takeAPDamage(int damage) {
+		
+		hp = hp - damage;
+	}
+	
+	void takeADDamage(int damage) {
+		
+		hp = hp - (defense - damage);
+	}
+	
 	void setAttack(int attack){
 		this.attack = attack;
+	}
+	
+	void drop(MainCharacter c) {
+		
+		for(Item e: drops) {
+			
+			c.addItem(e);
+		}
 	}
 	
 	void setDefense(int defense) {
