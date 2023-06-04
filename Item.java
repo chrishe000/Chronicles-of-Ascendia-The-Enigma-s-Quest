16b5bcf998970943db85e1274bcb795d1f62a36a
chrishe000 <134239835+chrishe000@users.noreply.github.com>
2023-06-04 11:54:42 -0700

    Update Item.java

--- Item.java
+++ Item.java
@@ -1,18 +1,12 @@
 // Author: Chris He
 // Date: 5/29/2023
 
-/*
-AdventureGameItem sword = new AdventureGameItem("Sword", "A sharp weapon for battles");
-System.out.println(sword.getName()); // Output: Sword
-System.out.println(sword.getDescription()); // Output: A sharp weapon for battles
-
-sword.use();    // Output: Using Sword...
-*/
-
 public class Item {
     private String name;
     private String description;
     private int cost;
+    private int ap;
+    private int ad;
 
     public Item(String name, String description) {
         this.name = name;
@@ -22,28 +16,50 @@ public class Item {
     public String getName() {
         return name;
     }
-
-    public String getDescription() {
-        return description;
-    }
-    
-    public int getCost() {
-        return cost;
-    }
     
     public void setName(String name) {
         this.name = name;
     }
 
+    public String getDescription() {
+        return description;
+    }
+    
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public int getCost() {
+        return cost;
     }
     
     public void setCost(int cost) {
         this.cost = cost;
     }
+    
+    public int getAP() {
+        return ap;
+    }
+    
+    public void setAP(int ap) {
+        this.ap = ap;
+    }
+    
+    public int getAD() {
+        return ad;
+    }
+    
+    public void setAD(int ad) {
+        this.ad = ad;
+    }
 
-    public void use() {
-        System.out.println("Using " + name + "...");
+    public void useHealPotion(MainCharacter m) {
+        m.hp = 0;
+    }
+    
+    public void useBoost(MainCharacter m, int ad, int ap, int def) {
+        m.attackDamage += ad;
+        m.abilityPower += ap;
+        m.defense += def;
     }
 }
