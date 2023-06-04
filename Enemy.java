0c0114f2732c65a8ce29a5158eff0eb368f52ebf
chrishe000 <134239835+chrishe000@users.noreply.github.com>
2023-06-04 12:27:24 -0700

    Update Enemy.java

--- Enemy.java
+++ Enemy.java
@@ -1,70 +1,77 @@
+// Author: Nishad Yedunuthula, Chris He
+// Date: 6/4/2023
+
 import java.util.ArrayList;
 
-//Nishad Yedunuthula
 public class Enemy {
-	
-	int attack;
+	int attackDamage;
+	int abilityPower;
 	int defense;
 	String name;
 	int hp;
 	ArrayList<Item> drops;
 	boolean isAlive;
 	
-	public Enemy(int attack, int defense, String name, int hp, ArrayList<Item> drops) {
-		
-		this.attack = attack;
+	public Enemy(int attackDamage, int abilityPower, int defense, String name, int hp, ArrayList<Item> drops) {
+		this.attackDamage = attackDamage;
+		this.abilityPower = abilityPower;
 		this.defense = defense;
 		this.name = name;
 		this.hp = hp;
 		this.drops = drops;
-		
 	}
-	
+
+	// attack combined
 	int attackPlayer(MainCharacter player, int attack) {
-		
 		int damage = player.getDefense() - attack;
+		
 		return damage;
 	}
 	
+	// end game
 	void die() {
-		if(hp == 0)
+		if(hp == 0) {
 			isAlive = false;
+		}
 	}
 	
-	void takeAPDamage(int damage) {
-		hp = hp - damage;
-	}
-	
-	void takeADDamage(int damage) {
-		hp = hp - (defense - damage);
-	}
-	
-	void setAttack(int attack){
-		this.attack = attack;
-	}
-	
+	// drop inventory
 	void drop(MainCharacter c) {
-		
 		for(Item e: drops) {
-			
 			c.addItem(e);
 		}
+	}
+	
+    // attack damage
+    public int getAttackDamage() {
+        return attackDamage;
+    }
+    
+    public void setDamage(int attackDamage) {
+        this.attackDamage = attackDamage;
+    }
+    
+    // ability power
+    public int getAbilityPower() {
+        return abilityPower;
+    }
+    
+    public void setAbilityPower(int abilityPower) {
+        this.abilityPower = abilityPower;
+    }
+
+	// defense
+	int getDefense() {
+		return defense;
 	}
 	
 	void setDefense(int defense) {
 		this.defense = defense;
 	}
 	
+	// hp
 	void setHp(int hp) {
 		this.hp = hp;
-	}
-	
-	int getAttack() {
-		return attack;
-	}
-	
-	int getDefense() {
-		return defense;
 	}
 
 	int getHp() {
