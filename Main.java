7f5402fa9d30f17e12ca92146de8faad7a429705
Franklin Wang <50060084+franklinwangg@users.noreply.github.com>
2023-06-04 12:39:05 -0700

    graphics adjustments
    moved jbuttons to near the bottom of the screen, added jlabels to display character stats at top of screen

--- Main.java
+++ Main.java
@@ -1,6 +1,11 @@
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.awt.Graphics;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.util.ArrayList;
 
+import javax.swing.Box;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -8,26 +13,30 @@ import javax.swing.JPanel;
 
 public class Main extends JPanel {
 
-	int width = 100, height = 100;
+	int width = 300, height = 300;
 	ArrayList<Dungeon> dungeons;
 	MainCharacter character;
 	
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
+		Main m = new Main();
+		
+		
 		frame.setVisible(true);
-		frame.setBounds(0,0,100,100);
+		frame.setBounds(0,0,m.width, m.height);
 
 
-		frame.add(new Main());
+		frame.add(m);
 	}
 	
 	public Main() {
 		this.dungeons = new ArrayList<Dungeon>();
-		String[] options = {"hello", "say hello"};
+		String[] options = {"hello", "say hello", "ads"};
 		ArrayList<Character> monsterList = new ArrayList<Character>();
 		Dungeon dungeon1 = new Dungeon("ez dungeon", 1, monsterList, options);
 		dungeons.add(dungeon1);
-		MainCharacter character = new MainCharacter(0,0,0,0);
+		this.character = new MainCharacter(0,0,0,0);
+		System.out.println("FASD");
 	}
 	
 	public Main(Dungeon[] dungeons, MainCharacter c) {
@@ -62,29 +71,34 @@ public class Main extends JPanel {
 	}
 	
 	public void paintDungeon(Dungeon d, Graphics g) {
-		this.add(new JLabel(/*get the text inside dungeon d*/));
-		
-		// based on how many options are inside Dungeon d,
-		// create that many JButtons with their respective texts on it
-		int numberOfOptions = d.getOptions().length;
-		System.out.println(numberOfOptions);
-		int heightOfButton = height / numberOfOptions;
-		int widthOfButton = width / numberOfOptions;
-		
-		for(int i = 0; i < numberOfOptions; i ++) {
-			this.add(new JButton(d.getOptions()[i]));
-		}
-		
-//		int count = 0;
-//		
-//		
-//		for(int i = 0; i < width; i += widthOfButton) {
-//			for(int j = 0; j < height; j += heightOfButton) {
-//				this.add(new JButton(d.getOptions()[count]));
-//				count ++;
-//			}
-//		}
-		
+	    this.setLayout(new BorderLayout()); // Set the layout manager before adding components
+
+//	    this.add(new JLabel(/*get the text inside dungeon d*/), BorderLayout.NORTH);
+	    this.add(new JLabel("asdkf"), BorderLayout.NORTH);
+
+	    paintStats();
+	    
+	    JPanel buttonPanel = new JPanel();
+	    buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+
+	    int numberOfOptions = d.getOptions().length;
+
+	    for (int i = 0; i < numberOfOptions; i++) {
+	        buttonPanel.add(new JButton(d.getOptions()[i]));
+	    }
+
+	    int height = this.getHeight();
+	    int targetY = (int) (height * 0.25);
+
+	    this.add(Box.createVerticalStrut(targetY), BorderLayout.CENTER);
+	    this.add(buttonPanel, BorderLayout.SOUTH);
+	}
+
+
+	public void paintStats() {
+	    this.add(new JLabel("hp : " + character.hp + ", basic attack damage : " 
+	+ character.attackDamage + ", ability power : " + character.abilityPower), 
+	    		BorderLayout.NORTH);
 	}
 	
 	public Dungeon randomDungeon(ArrayList<Dungeon> d) {
