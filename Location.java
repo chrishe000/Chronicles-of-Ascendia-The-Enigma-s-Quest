2974ff8f2ab4f7c65029123f7206dfb51f3dd789
Franklin Wang <50060084+franklinwangg@users.noreply.github.com>
2023-06-05 16:34:31 -0700

    added initializeMap() method to read in text files
    initializeMap only initializes the scenario for now

--- Location.java
+++ Location.java
@@ -1,17 +1,20 @@
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.Graphics;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList; // import the ArrayList class
 import java.util.Scanner;
 
-import Elements.Enemy;
-import Elements.TowerLand;
-import Level.Gridspace;
-import Projectile.Effect;
-import Projectile.Projectile;
+import javax.swing.Box;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
-public class Location {
-	
-	
+
+public class Location extends JPanel{
+
+
 	/*
 	 * planning : 
 
@@ -28,68 +31,135 @@ dungeon(String file) :
 	int difficulty;// the difficulty of the dungeon
 	ArrayList<Character> monsterList;// the list of monsters in this dungeon
 	private String[] options;
-	
+	String scenario;
+
 	public Location(String name, int difficulty, ArrayList<Character> monsterList, String[] options) {
-		
+
 		this.name = name;
-        this.difficulty = difficulty;
-        this.monsterList = monsterList;
-        this.options = options;
+		this.difficulty = difficulty;
+		this.monsterList = monsterList;
+		this.options = options;
+	}
+
+	public Location(String file) {
+		initializeMap(file);
+		System.out.println("scenario : " + scenario);
 	}
 	
 	void spawnList(){// sets the monsterlist that the dungeon has based on the difficulty of the dungeon
-		
-		if(difficulty == 1)
-			//
-		else if(difficulty == 2)
-			//	
-		else if(difficulty == 3)
-			//etc...
-			
 
-		
+		if(difficulty == 1) {}
+		//
+		else if(difficulty == 2) {}
+		//	
+		else if(difficulty == 3) {}
+		//etc...
+
+
+
 	}
-	
+
+
+	/*
+	 * i want the scanner to read in everything starting from 
+	 * SCENARIO to ENEMIES
+	 * 
+	 * from the end of SCENARIO to the start of ENEMIES
+	 * */
 	public void initializeMap(String filePath) {
-		{
-			int x = 0, y = 0;
-//			File randNums = new File("textfiles/randnums1");
-			File dungeonPlan = new File(filePath);
+		initializeScenario(filePath);
+	}
 
-			Scanner scanner;
+	private void initializeScenario(String filePath) {
 
-			try {
-				scanner = new Scanner(dungeonPlan);
+		File file = new File(filePath);
+		String overall = "";
+		String scenarioString = "SCENARIO : ";
+		String enemiesString = "ENEMIES : ";
+		int scenarioStringStart;
+
+		try {
+			Scanner scanner = new Scanner(file);
+
+			boolean started = false;
+
+			while (scanner.hasNextLine()) {
 
 
+				String line = scanner.nextLine();
+
+				if (line.contains(scenarioString)) {
+					started = true;
+					scenarioStringStart = line.indexOf(scenarioString) + scenarioString.length();
+					overall += line.substring(scenarioStringStart);
 				}
-			 catch (FileNotFoundException e) {
-				e.printStackTrace();
-			
+				else if(line.contains(enemiesString)) {
+					break;                	
+				}
+				else if(started == true) {
+					overall += line;
+				}
 			}
+
+			scanner.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 		}
+
+		System.out.println("overall : " + overall);
+		scenario = overall;
+		System.out.println("post method scenario : " + scenario);
+	}
+
+	public void paint(Graphics g, MainCharacter character) {
+		
+		this.setLayout(new BorderLayout()); // Set the layout manager before adding components
+
+		paintStats(character);
+
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+
+		int numberOfOptions = getOptions().length;
+
+		for (int i = 0; i < numberOfOptions; i++) {
+
+			JButton button = new JButton(getOptions()[i]);
+			button.setUI(new StyledButtonUI());
+			buttonPanel.add(button);
+		}
+
+		int height = this.getHeight();
+		int targetY = (int) (height * 0.25);
+
+		this.add(Box.createVerticalStrut(targetY), BorderLayout.CENTER);
+		this.add(buttonPanel, BorderLayout.SOUTH);
+
 	}
 	
+	public void paintStats(MainCharacter character) {
+		this.add(new JLabel("hp : " + character.getHP() + ", basic attack damage : " 
+				+ character.getAttackDamage() + ", ability power : " + character.getAbilityPower()), 
+				BorderLayout.NORTH);
+	}
+
 	void monsterDefeated() {// when the main character beats a monster, it is removed from the arraylist
-		
+
 		monsterList.remove(0);
 	}
-	
+
 	void setDungeon(ArrayList<Character> monsters) {// set method
-		
+
 		monsterList = monsters;
 	}
-	
+
 	ArrayList<Character> getDungeon(){// get method
-		
+
 		return monsterList;
 	}
-	
+
 	String[] getOptions() {
 		return options;
 	}
-	
-	
-	
-	
+
 }
