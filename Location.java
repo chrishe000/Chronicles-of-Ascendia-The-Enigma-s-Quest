50c6c76042bd199ef17892bddfdc989526142fcd
Franklin Wang <50060084+franklinwangg@users.noreply.github.com>
2023-06-05 12:52:59 -0700

    Rename Dungeon.java to Location.java

--- Location.java
+++ Location.java
@@ -1,13 +1,35 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList; // import the ArrayList class
+import java.util.Scanner;
 
-public class Dungeon {
+import Elements.Enemy;
+import Elements.TowerLand;
+import Level.Gridspace;
+import Projectile.Effect;
+import Projectile.Projectile;
+
+public class Location {
 	
+	
+	/*
+	 * planning : 
+
+read textfiles into dungeons, initialize the enemies and options in dungeon
+
+main : 
+	dungeons[] array
+	for each dungeon in dungeons[], initialize it
+
+dungeon(String file) : 
+	read file in
+	 * */
 	String name;// will be something like "LEVEL 1...2...3...etc"
 	int difficulty;// the difficulty of the dungeon
 	ArrayList<Character> monsterList;// the list of monsters in this dungeon
 	private String[] options;
 	
-	public Dungeon(String name, int difficulty, ArrayList<Character> monsterList, String[] options) {
+	public Location(String name, int difficulty, ArrayList<Character> monsterList, String[] options) {
 		
 		this.name = name;
         this.difficulty = difficulty;
@@ -26,6 +48,26 @@ public class Dungeon {
 			
 
 		
+	}
+	
+	public void initializeMap(String filePath) {
+		{
+			int x = 0, y = 0;
+//			File randNums = new File("textfiles/randnums1");
+			File dungeonPlan = new File(filePath);
+
+			Scanner scanner;
+
+			try {
+				scanner = new Scanner(dungeonPlan);
+
+
+				}
+			 catch (FileNotFoundException e) {
+				e.printStackTrace();
+			
+			}
+		}
 	}
 	
 	void monsterDefeated() {// when the main character beats a monster, it is removed from the arraylist
