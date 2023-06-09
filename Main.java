0fc5e0f9fc8cc9990b890d5a1f5692aeb05259f4
NISHADYHHS <62825462+NISHADYHHS@users.noreply.github.com>
2023-06-09 13:03:50 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,15 @@
-// Author: Franklin Wang, Nishad Yedunuthula
-// 6/4/2023
-
 import java.awt.BorderLayout;
+import java.awt.Button;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.RenderingHints;
 import java.util.ArrayList;
+
 import javax.swing.AbstractButton;
 import javax.swing.Box;
 import javax.swing.JButton;
@@ -18,6 +19,8 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.plaf.basic.BasicButtonUI;
+
+//Franklin Wang, Nishad Yedunuthula
 
 class StyledButtonUI extends BasicButtonUI {
 
@@ -42,40 +45,45 @@ class StyledButtonUI extends BasicButtonUI {
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         g.setColor(new Color(200, 200, 200));
         g.fillRoundRect(0, yOffset, size.width, size.height - yOffset, 10, 10);
+        
         g.setColor(new Color(244, 244, 244));
         g.fillRoundRect(0, yOffset, size.width , size.height + yOffset - 3, 10, 10);
     }
 }
 
 public class Main extends JPanel {
+
 	int width = 300, height = 300;
-	ArrayList<Dungeon> dungeons;
+	ArrayList<Location> dungeons;
 	MainCharacter character;
 	
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
 		Main m = new Main();
 		
+		
 		frame.setVisible(true);
 		frame.setBounds(0,0,m.width, m.height);
+
 
 		frame.add(m);
 	}
 	
 	public Main() {
-		this.dungeons = new ArrayList<Dungeon>();
+		this.dungeons = new ArrayList<Location>();
 		String[] options = {"hello", "say hello", "ads"};
-		ArrayList<Character> monsterList = new ArrayList<Character>();
-		Dungeon dungeon1 = new Dungeon("ez dungeon", 1, monsterList, options);
+		ArrayList<Enemy> monsterList = new ArrayList<Enemy>();
+		Location dungeon1 = new Location("ez dungeon", 1, monsterList, options);
 		dungeons.add(dungeon1);
 		this.character = new MainCharacter(0,0,0,0,0, 0);
-		System.out.println("FASD");
 	}
 	
-	public Main(Dungeon[] dungeons, MainCharacter c) {
+	public Main(Location[] dungeons, MainCharacter c) {
 //		while(c.isAlive) {
 //			
 //		} 
+		
+		 
 		
 		/*
 		 * while the game is continuing
@@ -89,16 +97,19 @@ public class Main extends JPanel {
 		 * for each dungeon in DUNGEONS write its text
 		 on the jframe 
 		 
+		 
 		 * */
 	}
 	
 	public void paint(Graphics g) {
 		super.paint(g);
-		Dungeon randomDungeon = randomDungeon(dungeons);
+		Location randomDungeon = randomDungeon(dungeons);
 		paintDungeon(randomDungeon, g);
+		
+		
 	}
 	
-	public void paintDungeon(Dungeon d, Graphics g) {
+	public void paintDungeon(Location d, Graphics g) {
 	    this.setLayout(new BorderLayout()); // Set the layout manager before adding components
 
 //	    this.add(new JLabel(/*get the text inside dungeon d*/), BorderLayout.NORTH);
@@ -127,17 +138,18 @@ public class Main extends JPanel {
 
 
 	public void paintStats() {
-	    this.add(new JLabel("hp : " + character.getHP() + ", basic attack damage : " 
-	+ character.getAttackDamage() + ", ability power : " + character.getAbilityPower()), 
+	    this.add(new JLabel("hp : " + character.getHP() + "\n AD: " 
+	+ character.getAttackDamage() + "\n AP : " + character.getAbilityPower()), 
 	    		BorderLayout.NORTH);
 	}
 	
-	public Dungeon randomDungeon(ArrayList<Dungeon> d) {
+	public Location randomDungeon(ArrayList<Location> d) {
 		int randNum = (int)(d.size() * Math.random());
 		return d.get(randNum);
 	}
 	
-	public void run(Dungeon[] dungeons, MainCharacter c) {
+	public void run(Location[] dungeons, MainCharacter c) {
 		
-	}	
+	}
+	
 }
